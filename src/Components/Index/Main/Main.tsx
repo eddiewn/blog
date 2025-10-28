91c2c73a45aebea039212ad4dc4b5bdf36523d6f
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2025-10-28 11:41:54 +0100

    Spacing added on post of the day

--- src/Components/Index/Main/Main.tsx
+++ src/Components/Index/Main/Main.tsx
@@ -3,14 +3,14 @@ import Aside from "./Aside/Aside";
 const Main = () => {
     return(
         <>
-            <main className="flex flex-col w-9/10 m-auto bg-amber-900 p-2">
+            <main className="flex flex-col lg:flex-row w-9/10 m-auto bg-amber-900 p-2">
                 <section className="w-full lg:w-3/4">
                     <h1>POST OF THE DAY</h1>
-                    <article className="bg-gray-400 w-full">
+                    <article className="bg-gray-400 w-full p-3">
                         <h2>PLACEHOLDER TITLE</h2>
                         <p>This is placeholder summary summary summary summary summary summary summary summary summary summary summary summary summary summary summary </p>
                         <img src="placeholder.png" alt="" />
-                        <button className="btn">VIEW</button>
+                        <button className="btn mt-1">VIEW</button>
                     </article>
                 </section>
                 <Aside />
