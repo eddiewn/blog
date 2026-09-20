65c5cd8ca70e762bbcc3067cced2e91850764afa
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2026-09-20 19:08:10 +0200

    fu cors

--- server.ts
+++ server.ts
@@ -82,7 +82,7 @@ app.set("trust proxy", 1);
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://blog.eddiewiiknilsson.com",
         credentials: true,
     }),
 );
